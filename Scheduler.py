2417ade7b87030f691657a62a3b5a5aa744e989f
TylerNichols <tnichols@udel.edu>
2016-12-06 16:10:53 -0500

    Added jobs adding to queues
    Jobs are now added to queues based on priority. Jobs that require more
    memory or devices than the system has are rejected and not added to any
    queues.

--- Scheduler.py
+++ Scheduler.py
@@ -1,5 +1,6 @@
 import sys
 import string
+import pprint
 
 # Class to handle queues
 class Queue:
@@ -20,6 +21,21 @@ class Queue:
 
     def get_items(self):
         return self.items
+
+    def get_shortest_job(self):
+        shortest = []
+        minruntime = 999999;
+
+        if self.size() == 0:
+            return
+
+        for job in self.items:
+            if job.runTime < minruntime :
+                minruntime = job.runTime
+                shortest = job
+
+        return shortest
+
 
 # Class to represent the system
 class System:
@@ -63,37 +79,31 @@ def main():
 
     print("Scheduler is done")
 
+
 # Dispatch function to handle all lines
 def process(line):
     parameters = ['C', 'A', 'Q', 'L', 'D']
     if line[0] in parameters:
-
-        check(line)
+        tick(line)
         print("Processing line " + line)
         if line[0] == 'C' :
             print("processing config")
             setup_system(line)
-
         if line[0] == 'A':
-
             print("processing job arrival")
-
+            process_job_arrival(line)
         elif line[0] == 'Q':
-
             print("processing device request")
-
         elif line[0] == 'L':
             print("processing device release")
-
         elif line[0] == 'D':
             print("processing display")
-
         return
     else:
         print("line was invalid .... \"" + line + "\"")
 
 # checks and compares arrival times
-def check(line):
+def tick(line):
     global time
     args = line_to_args(line)
 
@@ -106,35 +116,54 @@ def check(line):
 ##ticks time equal to time difference if first received
 def tick_start_time(timediff):
     global time
+    print("ticking till system start time...")
     for _ in range(timediff):
         time += 1
-        print(time)
+        print( time)
 
 #ticks time equal to time difference
 def tick_time(timediff):
     global time
     for _ in range(timediff):
-        time+=1
-        updateprocesses()
-        checkqueue()
+        time += 1
+        update_processes()
+        check_queues()
         print(time)
 
 
-#processes an arrived job IN PROGRESS
-def processjobarrival(line):
+# processes an arrived job IN PROGRESS
+def process_job_arrival(line):
+    global total_system
+    args = line_to_args(line)
+    currentjob = Job(args[0], args[1], args[2], args[3], args[4], args[5])
+
+    # Do not process jobs if they require more memory or devices than the system has
+    if (currentjob.devices > total_system.devices) or (currentjob.memory > total_system.memory):
+        return;
+
+    # put priority 1 jobs in queue 1, priority 2 in queue 2
+    elif currentjob.priority == "1":
+        total_system.holdqueue1.enqueue(currentjob)
+    else:
+        total_system.holdqueue2.enqueue(currentjob)
+
     print("job processed")
 
-#processes an arrived request IN PROGRESS
-def processrequest(line):
+
+# processes an arrived request IN PROGRESS
+def process_request(line):
     print("request processed")
 
-#processes updated IN PROGRESS
-def updateprocesses():
+
+# processes updated IN PROGRESS
+def update_processes():
     print("processes updated")
 
-#checks queue IN PROGRESS
-def checkqueue():
-    print("queue checked")
+
+# checks queue IN PROGRESS
+def check_queues():
+    print("queues checked")
+
 
 # Setup system with specified args
 def setup_system(line):
@@ -145,7 +174,10 @@ def setup_system(line):
 
 
 def display_system(line):
+    global total_system
     print("CURRENT STATUS OF SYSTEM")
+    pprint(total_system)
+
 
 # Turn a line with letters/numbers/etc in to an array
 # that can be used to fill classes. Produced arrays
---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+python Scheduler.py test.txt
+pause
